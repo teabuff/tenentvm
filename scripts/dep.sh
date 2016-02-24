5b1b6e2a26a7d0e9aa2a401da61c35407f2e86ba
Zhou Jiang <i@jiangzhou.us>
2016-02-24 11:47:33 -0800

    django dev

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -7,3 +7,10 @@ apt-get -y update
 apt-get -y install curl
 
 # You can install anything you need here.
+apt-get -y install python3
+
+apt-get -y install python3-pip
+
+pip install --upgrade pip
+
+pip install django
